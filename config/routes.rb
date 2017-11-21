496b63e5d1460d4fa5584a0d01b0be8f869490ca
Lola Barbier <lolabarbier@hotmail.com>
2017-11-21 12:59:40 +0100

    made model, controller, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new'
+
+  post 'create', to: 'tasks#create'
+
+  get 'tasks/:id', to: 'tasks#show'
+
+  get 'tasks/:id/edit', to: 'tasks#edit'
+
+  patch 'tasks/:id', to: 'tasks#update'
+
+  get 'tasks/:id', to: 'tasks#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
